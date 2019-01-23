1198fd8cea90b5057a84c0ce91d80872fbe957fc
JTerry <jessica.terry79@yahoo.com>
2019-01-22 20:39:19 -0600

    finished Card.js file

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,3 +1,15 @@
 // Import the required files and dependencies
 import React from "react";
 import "./Card.css";
+
+// Set up card layout using props
+const Card = props => (
+    <div className="card" onClick={() => props.clickCount(props.id)}>
+        <div className="img-container">
+            <img alt={props.name} src={props.image} />
+        </div>
+    </div>
+);
+
+// Export the Cards
+export default Card;
