508bde5cb63e4d163ef87467659e1c08a09654b3
JTerry <jessica.terry79@yahoo.com>
2019-01-22 21:08:09 -0600

    finished Header.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+// Import the required dependencies and files
+import React from "react";
+import "./Header.css";
+
+// Set up the header for the clicky game app using props
+const Header = props => (
+    <div className="header">
+        <div className="title">{props.childer}</div>
+        <div className="scores">
+            Score: {props.score} Highscore: {props.highscore}
+        </div>
+    </div>
+);
+
+// Export the Header to the app
+export default Header;
