6dfa99dee109762b540541b0eaab8749ff83c619
JTerry <jessica.terry79@yahoo.com>
2019-01-25 00:33:54 -0600

    Updates

---
+++ precache-manifest.114f896477a9f008e75b41807ac4ffa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55abe30a270b3be75741",
+    "url": "/react-clicky-game/static/js/runtime~main.55abe30a.js"
+  },
+  {
+    "revision": "d2a1d09c000804258087",
+    "url": "/react-clicky-game/static/js/main.d2a1d09c.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/react-clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "d2a1d09c000804258087",
+    "url": "/react-clicky-game/static/css/main.581f41f1.chunk.css"
+  },
+  {
+    "revision": "b26d6dab52e7d854425f101b7815201d",
+    "url": "/react-clicky-game/index.html"
+  }
+];
